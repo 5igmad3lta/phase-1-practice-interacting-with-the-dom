17663d5bbdff831d42ca6ca2c797aec43e1352ef
Stavros Droulias <stavros.d27@gmail.com>
2024-12-20 17:51:38 +1100

    complete lab

--- js/challenge.js
+++ js/challenge.js
@@ -45,32 +45,41 @@ function repeatCounter() {
     intervalId2 = setInterval(resetToZero, 1000)
 }
 repeatCounter();
-function heartTime() {  
-    likeCount++;
-    let newLike = document.createElement('li');
-    if(likeCount = 1) {
-    newLike.textContent = `${count} was liked ${likeCount} time`
-    document.querySelector('.likes').appendChild(newLike);
-    } else {
-        likeCount++;
-         newLike.textContent = `${count} was liked ${likeCount} time`
-    document.querySelector('.likes').appendChild(newLike);
+function heartTime() {
+    const newList = document.createElement('li');
+    newList.setAttribute('data-num', `${count}`)
+    const textBeforeSpan = document.createTextNode(`${count} has been liked `)
+    const span = document.createElement('span');
+    span.textContent = 1;
+    const textAfterSpan = document.createTextNode(' time');
+    newList.appendChild(textBeforeSpan);
+    newList.appendChild(span);
+    newList.appendChild(textAfterSpan);
+    console.log(newList);
+    const likeArray = Array.from(document.querySelectorAll('[data-num]'))
+    console.log(newList.dataset.num)
+        if (likeArray.find(item => item.dataset.num == newList.dataset.num)) {
+            let existingList = document.querySelector(`[data-num= "${newList.dataset.num}"]`)
+            let spanCount = parseInt(existingList.querySelector('span').textContent, 10)
+            spanCount++;
+            existingList.querySelector('span').textContent = spanCount;
+            if (spanCount <= 2) {
+                existingList.innerHTML = existingList.innerHTML.replace('time', 'times')
+            }
+        } else { 
+            document.querySelector('.likes').appendChild(newList);
+            console.log('This is a fresh post')};
+    };
 
-    }
+function leaveAComment(event) {
+    event.preventDefault()
+    let newComment = document.createElement('p');
+    newComment.textContent = document.getElementById('comment-input').value;
+    document.querySelector('.comments').appendChild(newComment);
+    document.getElementById('comment-form').reset();
 }
 
-
-
-
-//     if(likeCount === 1) {
-//         newLike.textContent = `${count} has been liked ${likeCount} time`;
-//         document.querySelector('.likes').appendChild(newLike);
-//         console.log(likeCount)
-//     } else {
-//         newLike.textContent = `${count} has been liked ${likeCount} times`;
-//         document.querySelector('.likes').appendChild(newLike);
-//     }
-// }
+let postComment = document.getElementById('submit').addEventListener('click', leaveAComment)
 let heartBtn = document.getElementById('heart').addEventListener('click', heartTime)
 let pauseBtn = document.getElementById('pause').addEventListener('click', pauseTimer)
 let minusBtn = document.getElementById('minus').addEventListener('click', decreaseTime)
